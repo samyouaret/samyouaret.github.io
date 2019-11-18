278dde2ed8c3d6c2c0f75cd01b30698af9f60c38
samyouaret <samyouaret13@gmail.com>
2019-11-18 20:56:02 +0100

    add about page

--- src/js/components/About.js
+++ src/js/components/About.js
@@ -3,6 +3,9 @@ import React from 'react';
 function About() {
   return (<div>
     <h2>About page</h2>
+    <p className="lead">I am a web developer, love developing using php,laravel and react,
+      im also passionate about football.</p>
+    <p className="lead">I develope web application using php and laravel,</p>
   </div>
   );
 }
