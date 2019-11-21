d4292ba6edb8e4ab105a8f4497dbb8ba92217117
samyouaret <samyouaret13@gmail.com>
2019-11-21 14:46:35 +0100

    add contact page

---
+++ src/js/components/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Contact() {
+  return (<div>
+    <h2>Get in touch with me</h2>
+    <p className="lead">You can find on social media or on github</p>
+  </div>
+  );
+}
+
+export default Contact;
--- src/js/components/ContentBrowser.js
+++ src/js/components/ContentBrowser.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Writings from './Writings';
-import About from './About';
+import Contact from './Contact';
 import Resume from './Resume';
 import Home from './Home';
 import NotFound from './NotFound';
@@ -13,9 +13,9 @@ function ContentBrowser() {
         <div className="content">
             <Switch>
                 <Route path='/' exact component={Home} />
-                <Route path='/about' component={About} />
-                <Route path='/Writings' component={Writings} />
-                <Route path='/Resume' component={Resume} />
+                <Route path='/writings' component={Writings} />
+                <Route path='/resume' component={Resume} />
+                <Route path='/getintouch' component={Contact} />
                 <Route component={NotFound} />
             </Switch>
         </div>
